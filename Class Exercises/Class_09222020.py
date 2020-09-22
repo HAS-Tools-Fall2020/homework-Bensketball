23d404f3302c3014a3efb444973abf86a4e3199b
Ben <Bensketball343@gmail.com>
2020-09-22 08:35:11 -0700

    In class

---
+++ Class Exercises/Class_09222020.py
@@ -0,0 +1,26 @@
+# Class Exercises
+
+# %%
+# Import the modules we will use
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# EXERCISE 1: 
+#1a.  Create a 3X3 matrix with values ranging from 2-10
+
+# Laura
+# x = np.arange(2,11)
+# np.reshape(x)
+# print(x)
+
+#Richard
+print(np.array(range(2,11,1).reshape(3,3)))
+
+#Diana
+# Ex_1= np.array([(2,3,4),(5,6,7),(8,9,10)])
+# print(Ex_1)
+
+# %%
