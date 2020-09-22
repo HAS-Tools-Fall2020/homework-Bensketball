8a9c162d57d12411371453f3b577fcd4ed2ae0df
Ben <Bensketball343@gmail.com>
2020-09-22 13:03:41 -0700

    Class_09222020

--- Class Exercises/Class_09222020.py
+++ Class Exercises/Class_09222020.py
@@ -16,11 +16,33 @@ import matplotlib.pyplot as plt
 # np.reshape(x)
 # print(x)
 
-#Richard
-print(np.array(range(2,11,1).reshape(3,3)))
+# Richard
+print((np.array(range(2,11,1)).reshape(3,3)))
 
-#Diana
-# Ex_1= np.array([(2,3,4),(5,6,7),(8,9,10)])
-# print(Ex_1)
+# Diana
+Ex_1= np.array([(2,3,4),(5,6,7),(8,9,10)])
+print(Ex_1)
+
+
+#1.b  Make a matrix with all of the even values from 2-32
+
+# Ben
+print((np.array(range(1,17,1)).reshape(4,4))*2)
+
+
+# 1.c Make a matrix with all of the even values from 2-32
+# But this time have the values arrange along columns rather than rows
+
+# Ben
+print(np.transpose((np.array(range(1,17,1)).reshape(4,4))*2))
+
+
+# BONUS:
+# Create the same 3x3 matrix with value ranging from 2-10 as you did 
+# in part a but this time do so by combining one 3X1 matrix and one 1X3 matrix
+
+
 
 # %%
+# Notes:
+# This class explains how to Evaluate! (09222020)
