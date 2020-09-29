66598debfb8d9e3b94c9fabc8ba8603dce11b537
Ben <Bensketball343@gmail.com>
2020-09-29 08:23:33 -0700

    class 09292020

---
+++ Class Exercises/Class_09292020.py
@@ -0,0 +1,22 @@
+# Class Exercises
+
+# %%
+# Import the modules we will use
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# A) Change 
+
+
+# %%
+# C) Make the dataframe into a checkerboard  of 0's and 1's using loc
+
+# Ben
+data = np.ones((7,3))
+dataframe = pd.DataFrame(data, columns=['data1', 'data2', 'data3'],
+
+#%%
+# D) Do the same thing without using loc
