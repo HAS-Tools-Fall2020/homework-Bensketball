65d9f43f8f646dabeb0dd9336af7d3bb286feb91
Ben <Bensketball343@gmail.com>
2020-10-09 23:02:38 -0700

    how many things changed?

--- Submissions/Code_Review1/mitchell_HW6.md
+++ Submissions/Code_Review1/mitchell_HW6.md
@@ -17,58 +17,73 @@ ___
 `
 Just kidding, I can walk you through what I have created here.
 
-First off, the folder you copied to your computer should be the folder titled 'Code_Review1' found in my 'Submissions' folder.  Contained in this folder is my python code title 'mitchell_HW7.py' via the instructions on GitHub.  The website to this GitHub can be found [here]().  If you go to this website, you will find many helpful links to run my code.  The steps are below:
+First off, the folder you copied to your computer should be the folder titled 'Code_Review1' found in my 'Submissions' folder of GitHub Repo.  Contained in this folder is my python code title 'mitchell_HW7.py' via the instructions on GitHub.  There should also be an empty folder called graphs and an empty The website to this GitHub can be found [here](https://github.com/HAS-Tools-Fall2020/homework-Bensketball/tree/master/Submissions/Code_Review1) or [here]().  If you go to this website, you will find many helpful links to run my code.  The steps are below:
 
-Step #1: Download the daily streamflow data for station *09506000 Verde River Near Camp Verde* from the USGS NWIS [mapper](https://maps.waterdata.usgs.gov/mapper/).  Make sure you update your end date so you are getting the latest data to help you make your short term forecasts.  
+###### Step #1:
+Download the daily streamflow data for station *09506000 Verde River Near Camp Verde* from the USGS NWIS [mapper](https://maps.waterdata.usgs.gov/mapper/).  Make sure your click the 'Daily Data' link after selecting the gage number so that the model is up to date with the latest data to help make the short term forecasts.
    - Parameter 00060 Discharge (mean)
    - Start date = 1989-01-01
-   - End date = 2020-10-10
+   - End date = 2020-10-10 (Saturday this week)
    - Select 'tab separated'
 
+Save a text file in the folder titled 'data' and title this text file 'streamflow_week7.txt'.  The name chosen is critical to my code.
+
+P.S. There is already a text file titled 'streamflow_week7_temp.txt' in the 'data' folder.  This text file is missing this Saturday.  This is why you are updating the data. :)
+
+###### Step #2:
+Run my code.  Make sure that the filename in the beginning reads like this:
+```python
+filename = 'streamflow_week7.txt'
+filepath = os.path.join('data', filename)
+print(os.getcwd())
+print(filepath)
+```
+
+You will get
+
+ValueError: illegal value in 4-th argument of internal None
+
+
+~ Thank you!
 ___
 <a name="rev"></a>
 ### Your Code Review
-##### Fill in the question below from GitHub assignment.
+##### Fill in the question(s) below from GitHub assignment.
 ##### *Feel free to change up this space for your review of my code!  You do not need to follow my formatting.
 
 1. First check below to see who your partner is.
 
-  **Ans:**
-  `
-  `
-
-2. Clone their repo to your computer. You can find their repo by going to the main GitHub organization page for this class.
-
-  **Ans:**
-  `
-  `
+2. Clone their repo to your computer. You can find their repo by going to the [main GitHub organization page](https://github.com/HAS-Tools-Fall2020) for this class.
 
 3. Review their code following the code_review_rubric.md instructions in the starter_codes folder. You should provide your comments and scores in the spaces they have provided for you in their ReadMe.md file in the Code_Review1 folder of their GitHub submissions folder.
 
-  **Ans:**
+  This is the space provided.  Feel free to change up this space for your review of my code!  You do not need to follow my formatting.
+
+  **Ans**
   `
   `
 
 4. Follow the instructions they have provided in their ReadMe.md file to run their script and generate their 4 forecast values.
 
-  **Ans:**
-  `
-  `
-
 5. Enter all four values (the two regression forecasts and the two forecast values into the ReadMe.md file of your partners repo.
 
-  **Ans:**
+  **Ans1**
+  `
+  `
+
+  **Ans2**
+  `
+  `
+
+  **Ans3**
+  `
+  `
+
+  **Ans4**
   `
   `
 
 6. Enter the two forecast values into the lastname.csv file for your partner. Remember you are entering their forecast for them this week, not your own!
-
-  **Ans:**
-  `
-  `
-
-
-
 
 ___
 <a name="cod"></a>
@@ -76,50 +91,8 @@ ___
 
 ##### The Python code can be found [here](../Code_Review1/mitchell_HW7.py).  It can also be found the folder you downloaded.
 
-###### Review the starter code I provided to see how to build an autoregressive(AR) model. Then build your own model, you can modify my model in any way for example changing the number of time steps used for prediction or changing the testing and training data periods. The only rule is that you must make some modifications to make the model your own.
-
-###### For your written assignment provide the following. Your submission should include at least 3 different types of plots (see the note at the end of the previous section for how to add these into your markdown file if you are not sure how to do that):
-
-1. A summary of the AR model that you ended up building, including (1) what you are using as your prediction variables, (2) the final equation for your model and (3) what you used as your testing and training periods. In your discussion please include graphical outputs that support why you made the decisions you did with your model.
-
-**Ans:**
 
 
-`(2) "y = m * x + b" OR "y = 0.96 * x + 5.56" with an R_squared value of 0.91.
-`
-
-`(3) I choose to use every other day because I was cutting of a huge chunk of data when I restricted all flows to be under 200.  I made this choice because we seem to be in a drought right now.  The graphs below show my training period for my model and my full flow period:
-`
-
-![g-all](../assignment_6/graphs/Observed_Flow_All.png "Observed_Flow_All")
-
-![g-train](../assignment_6/graphs/Observed_Flow_Train.png "Observed_Flow_Train")
-
-
-2. Provide an analysis of your final model performance. This should include at least one graph that shows the historical vs predicted streamflow and some discussion of qualitatively how you think your model is good or bad.
-
-**Ans:**
-`I think this model can be greatly improved upon.  I think next time I want to use week's average flows instead of days but I will need to write a more complicated code for this.  I found that if I cut data off at 200 CFS that I got "NaN" values because I cut rows out of my data.  I do not think this model type is good for predicting flow because of how flow data seems rather random.  I suspect that this model is only good for no more than a week in advance.  I will say that because I cut my data off at 200 CFS that I ended up with a rather nice R_squared value of 0.91 (on its way to 1.00).  My graph(s) below show my model at work:
-`
-
-![g-sim](../assignment_6/graphs/Observed_Flow_Sim.png "Observed_Flow_Sim")
-
-![g-AR_Log](../assignment_6/graphs/AR_Log.png "AR_Log")
-
-![g-AR](../assignment_6/graphs/AR.png "AR")
-
-
-3. Finally, provide discussion on what you actually used for your forecast. Did you use your AR model, why or why not? If not how did you generate your forecast this week?
-
-**Ans:**
-`I went ahead and used the model to make my predictions.  However, I do not think that my current model is super accurate given the data I used.  Because I used a difference period of one day, using this model to predict weeks flows is inaccurate.  I generated my forecast by taking the formula spit out be the model "y = 0.96 * x + 5.56" and plugged in the flow over and over filling out my perditions until the end of the semester.  I used a "For" loop to do this.  The slope is positive so my predictions increase throughout the year.  Same goes for this weeks and next weeks predictions.  However, I used a different starting flow.  Using the week from 8-15-2020 to 8-21-2020, the average flow was calculated and used for my long term predictions.  This value was found to be 33.84285714285714.  For this weeks prediction, Saturday's (10/03/2020) flow was used for my iteration.
-`
-
-___
-<a name="est"></a>
-## Estimation6 Explanation
-
-Explanation is highlighted above in the questions this week. ^^^
 
 ___
 <a name="cod"></a>
--- assignment_7/week7_babys_first_code_BM.py
+++ assignment_7/week7_babys_first_code_BM.py
@@ -1,5 +1,9 @@
-# Starter code for week 6 illustrating how to build an AR model
-# and plot it
+# Baby's first autoregressive model
+# How to build an AR model, plot it
+# and use it for my perdiction
+
+# By Benjamin Mitchell
+
 
 # %%
 # Import the modules we will use
@@ -9,20 +13,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import datetime
+import math
+
 
 # %%
 # Set the file name and path to where you have stored the data
-# Make sure you are in the correct folder!!!
-filename = 'streamflow_week6_temp.txt'
+# Make sure you are in the correct folder!!! ~BM
+filename = 'streamflow_week7_temp.txt'
 filepath = os.path.join('data', filename)
 print(os.getcwd())
 print(filepath)
 
 
 # %%
-# Read the data into a pandas dataframe
+# Turn on an off the line below to delete the pandas frame
+# called 'Data' if you need to reset the data
 # del(data)
+# turn on the line above if 'data' needs to be reset
+# turn of when 'data' does not exist
 
+# Read the data into a pandas dataframe
 data = pd.read_table(filepath, sep='\t', skiprows=30,
                      names=['agency_cd', 'site_no', 'datetime', 'flow',
                             'code'], parse_dates=['datetime']
@@ -34,94 +44,62 @@ data['month'] = pd.DatetimeIndex(data['datetime']).month
 data['day'] = pd.DatetimeIndex(data['datetime']).day
 data['dayofweek'] = pd.DatetimeIndex(data['datetime']).dayofweek
 
-# print(data)
-# print(len(data))
-
 # Aggregate flow values to weekly
-# flow_weekly = data.resample("W", on='datetime').mean()
-
-# print(data[data['flow'] <= 200])
-data_200 = pd.DataFrame(data[data['flow'] <= 200])
-data_200 = data_200.set_index('datetime')
-data_200 = data_200.drop(columns=['agency_cd', 'code'])
-# print(data_200)
-
-flow_weekly = data_200
-# flow_weekly = data.resample(:, on='datetime')
-print(flow_weekly)
+flow_weekly = data.resample("W", on='datetime').mean()
+# As an added bonus I am taking the log of the data
+# becuase it fits the model better with all data included
+flow_weekly.insert(2, 'log_flow', np.log(flow_weekly['flow']), True)
+# print(flow_weekly)
 
 
 # %%
-# Ben's Code:
-# Ben's building an autoregressive model
-# You can learn more about the approach I'm following by walking
+# Building the autoregressive model
+# You can learn more about the approach I'm following by walking 
 # Through this tutorial
 # https://realpython.com/linear-regression-in-python/
 
 # Step 1: setup the arrays you will build your model on
 # This is an autoregressive model so we will be building
 # it based on the lagged timeseries
+flow_weekly['log_flow_tm1'] = flow_weekly['log_flow'].shift(1)
+flow_weekly['log_flow_tm2'] = flow_weekly['log_flow'].shift(2)
 
-flow_weekly['flow_tm1'] = flow_weekly['flow'].shift(1)
-flow_weekly['flow_tm2'] = flow_weekly['flow'].shift(2)
-
-# Step 2 - pick what portion of the time series you want to use
-# as training data
-# here I'm grabbing the first 800 weeks
+# Step 2: Pick what portion of the time series you want to use as training data
+# here I'm grabbing the last 334 weeks
+trainstart = 1300
+trainend = 1600
+print('trainstart', trainstart)
+print('trainend', trainend)
 # Note1 - dropping the first two weeks since they wont have lagged data
 # to go with them
-trainstart = 2
-trainend = 3000
-
-train = flow_weekly[trainstart:trainend][['flow', 'flow_tm1', 'flow_tm2']]
-test = flow_weekly[trainend:][['flow', 'flow_tm1', 'flow_tm2']]
+train = flow_weekly[trainstart:trainend][['log_flow',
+                                          'log_flow_tm1', 'log_flow_tm2']]
+test = flow_weekly[trainend:][['log_flow',
+                               'log_flow_tm1', 'log_flow_tm2']]
 
 # Step 3: Fit a linear regression model using sklearn
 model = LinearRegression()
+x = train['log_flow_tm1'].values.reshape(-1, 1)
 # See the tutorial to understand the reshape step here
-x = train['flow_tm1'].values.reshape(-1, 1)
-y = train['flow'].values
+y = train['log_flow'].values
 model.fit(x, y)
 
-# Look at the results
-# r^2 values
+# Step 3.5: Look at the results - r^2 values
 r_sq = model.score(x, y)
-print('coefficient of determination:', np.round(r_sq, 2))
+print('coefficient of determination:', np.round(r_sq, 7))
 
-# print the intercept and the slope
-print('intercept:', np.round(model.intercept_, 2))
-print('slope:', np.round(model.coef_, 2))
+# Print the intercept and the slope
+print('intercept:', np.round(model.intercept_, 7))
+print('slope:', np.round(model.coef_, 7))
 
-# Step 4 Make a prediction with your model
+# Step 4: Make a prediction with your model 
 # Predict the model response for a  given flow value
-q_pred_train = model.predict(train['flow_tm1'].values.reshape(-1, 1))
-q_pred_test = model.predict(test['flow_tm1'].values.reshape(-1, 1))
+q_pred_train = model.predict(train['log_flow_tm1'].values.reshape(-1,1))
+q_pred_test = model.predict(test['log_flow_tm1'].values.reshape(-1,1))
 
-# altrenatievely you can calcualte this yourself like this:
-q_pred = model.intercept_ + model.coef_ * train['flow_tm1']
+#altrenatievely you can calcualte this yourself like this: 
+q_pred = model.intercept_ + model.coef_ * train['log_flow_tm1']
 
-# you could also predict the q for just a single value like this
-last_week_flow = 500
-prediction = model.intercept_ + model.coef_ * last_week_flow
-
-
-# %%
-# Another example but this time using two time lags as inputs to the model
-model2 = LinearRegression()
-x2 = train[['flow_tm1', 'flow_tm2']]
-model2.fit(x2, y)
-r_sq = model2.score(x2, y)
-print('coefficient of determination:', np.round(r_sq, 2))
-print('intercept:', np.round(model2.intercept_, 2))
-print('slope:', np.round(model2.coef_, 2))
-
-# generate preditions with the funciton
-q_pred2_train = model2.predict(train[['flow_tm1', 'flow_tm2']])
-
-# or by hand
-q_pred2 = model2.intercept_   \
-         + model2.coef_[0] * train['flow_tm1'] \
-         + model2.coef_[1] * train['flow_tm2']
 
 # %%
 # Here are some examples of things you might want to plot to get you started:
@@ -130,80 +108,79 @@ q_pred2 = model2.intercept_   \
 # Note that date is the index for the dataframe so it will
 # automatically treat this as our x axis unless we tell it otherwise
 fig, ax = plt.subplots()
-ax.plot(flow_weekly['flow'], label='full')
-ax.plot(train['flow'], 'r:', label='training')
-ax.set(title="Observed Flow", xlabel="Date",
-       ylabel="Weekly Avg Flow [cfs]",
+ax.plot(flow_weekly['log_flow'], label='full')
+ax.plot(train['log_flow'], 'r:', label='training')
+ax.set(title="Observed Log(Flow)", xlabel="Date",
+       ylabel="Weekly Avg Log(Flow) [cfs]",
        yscale='log')
 ax.legend()
-# an example of saving your figure to a file
-fig.set_size_inches(5, 3)
-fig.savefig("graphs/Observed_Flow_All.png")
+# An example of saving your figure to a file
+fig.set_size_inches(6, 4.5)
+fig.savefig("graphs/Observed_Log(Flow)_All.png")
+
 
 # 2. Time series of flow values with the x axis range limited
 fig, ax = plt.subplots()
-ax.plot(flow_weekly['flow'], label='full')
-ax.plot(train['flow'], 'r:', label='training')
-ax.set(title="Observed Flow", xlabel="Date", ylabel="Weekly Avg Flow [cfs]",
-       yscale='log', xlim=[datetime.date(2000, 1, 26), datetime.date(2014,
+ax.plot(flow_weekly['log_flow'], label='full')
+ax.plot(train['log_flow'], 'r:', label='training')
+ax.set(title="Observed Log(Flow)", xlabel="Date", ylabel="Weekly Avg Log(Flow) [cfs]",
+       yscale='log', xlim=[datetime.date(2000, 1, 26), datetime.date(2019,
                                                                      2, 1)])
 ax.legend()
-fig.savefig("graphs/Observed_Flow_Train.png")
+fig.savefig("graphs/Observed_Log(Flow)_Train.png")
 
 
 # 3. Line  plot comparison of predicted and observed flows
 fig, ax = plt.subplots()
-ax.plot(train['flow'], color='grey', linewidth=2, label='observed')
+ax.plot(train['log_flow'], color='grey', linewidth=2, label='observed')
 ax.plot(train.index, q_pred_train, color='green', linestyle='--',
         label='simulated')
 ax.set(title="Observed Flow", xlabel="Date", ylabel="Weekly Avg Flow [cfs]",
        yscale='log')
 ax.legend()
-fig.savefig("graphs/Observed_Flow_Sim.png")
+fig.savefig("graphs/Observed_Log(Flow)_Sim.png")
 
 
 # 4. Scatter plot of t vs t-1 flow with log log axes
 fig, ax = plt.subplots()
-ax.scatter(train['flow_tm1'], train['flow'], marker='p',
+ax.scatter(train['log_flow_tm1'], train['log_flow'], marker='p',
            color='blueviolet', label='obs')
-ax.set(xlabel='flow t-1', ylabel='flow t', yscale='log', xscale='log')
-ax.plot(np.sort(train['flow_tm1']), np.sort(q_pred_train), label='AR model')
+ax.set(xlabel='Log(Flow) t-1', ylabel='Log(Flow) t', yscale='log', xscale='log')
+ax.plot(np.sort(train['log_flow_tm1']), np.sort(q_pred_train), label='AR model')
 ax.legend()
 fig.savefig("graphs/AR_Log.png")
 
+
 # 5. Scatter plot of t vs t-1 flow with normal axes
 fig, ax = plt.subplots()
-ax.scatter(train['flow_tm1'], train['flow'], marker='p',
+ax.scatter(train['log_flow_tm1'], train['log_flow'], marker='p',
            color='blueviolet', label='observations')
-ax.set(xlabel='flow t-1', ylabel='flow t')
-ax.plot(np.sort(train['flow_tm1']), np.sort(q_pred_train), label='AR model')
+ax.set(xlabel='Log(Flow) t-1', ylabel='Log(Flow) t')
+ax.plot(np.sort(train['log_flow_tm1']), np.sort(q_pred_train), label='AR model')
 ax.legend()
 fig.savefig("graphs/AR.png")
 
 plt.show()
 
+# All graphs are saved in the folder called graphs.
 
 # %%
-# Semester Predictions
+# Finding the average of last weeks Flow, follwed by the predictions.
 # y = mx + b
+week_before = flow_weekly['log_flow'].tail(1)
+print("Last weeks's flow was", math.exp(week_before),'!', '\n')
+# Finding next weeks and next next weeks flows useing model and a fuction
+def my_prediction(x):
+        for i in range(1, x):
+                week_bef = week_before
+                print('week_%i:'%(i))
+                log_pred = model.intercept_ + model.coef_ * week_bef
+                pred = math.exp(log_pred)
+                print(log_pred, '\n')
+                print('prediction #%i:'%(i), pred, '\n')
+                week_bef = log_pred
 
-week_before1 = flow_weekly.loc['20200815':'20200821'].flow.mean()
-print(week_before1)
-for i in range(1, 17):
-        print('week', [i])
-        pred_weeks1 = model.intercept_ + model.coef_ * week_before1
-        print(pred_weeks1)
-        week_before1 = pred_weeks1
-
-# %%
-# Finding Mondays Flow!
-# y = mx + b
-Saturdays_Flow = flow_weekly['flow'].tail(1)
-print(Saturdays_Flow)
-week_before2 = Saturdays_Flow
-for i in range(1, 3):
-        print('week1', [i])
-        pred1 = model.intercept_ + model.coef_ * week_before2
-        print(pred1)
-        week_before2 = pred1
+# The number chosen for the function named "my_prediction", was "3".
+# This is becuase we want to predict next weeks flow and next next weeks flow.
+my_prediction(3)
 # %%
