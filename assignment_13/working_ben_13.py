a7e75c5619e5a1c4b7d36e2e19db8aeda98409dc
Ben <Bensketball343@gmail.com>
2020-11-23 00:26:53 -0700

    Homework 13 Finished!

--- assignment_13/working_ben_13.py
+++ assignment_13/working_ben_13.py
@@ -33,10 +33,11 @@ def mono_reg_mod(test_weeks):
     coeff_det1 = np.round(reg_model.score(x_val_model1, y_val_model1), 7)  # r^2
     b = np.round(reg_model.intercept_, 7)  # Intercept
     m = np.round(reg_model.coef_, 7)  # Slope
+    q_pred_mono = reg_model.predict(test_weeks['log_flow_tm1'].values.reshape(-1,1))
     print('coefficient of determination:', np.round(coeff_det1, 7))
     # Intercept and the slope (Final equation) y= mx + b
     print('Final equation is y1 = :', m[:1], 'x + ', b)
-    return(b,m,reg_model,coeff_det1)
+    return(b,m,reg_model,coeff_det1,q_pred_mono)
 
 # Building a function for our Linear Regression Model
 def poly_reg_mod(test_weeks):
@@ -51,10 +52,11 @@ def poly_reg_mod(test_weeks):
     coeff_det2 = np.round(poly_model.score(x_val_model2, y_val_model2), 7)  # r^2
     c = np.round(poly_model.intercept_, 7)  # Intercept
     a = np.round(poly_model.coef_, 7)  # Slope(s)
+    q_pred_poly = poly_model.predict(test_weeks[['log_flow_tm1', 'log_flow_tm2']])
     print('coefficient of determination:', np.round(coeff_det2, 7))
     # Intercept and the slope (Final equation) y= a1*x1 + a2*x2 + c
     print('Final equation is y2 = :', a[:1], 'x1 + ', a[1:2], 'x2 + ', c)
-    return(c,a,poly_model,coeff_det2)
+    return(c,a,poly_model,coeff_det2,q_pred_poly)
 
 # Building a function for flow prediction outside of the AR model
 def real_prediction(indexnumber, last_week_flow, last2_week_flow=None):
@@ -127,6 +129,59 @@ def week_prediction_all(flow, m, b, week_pred, end, prev_wks):
     prediction = (b + m * flow_range_value)*(1) # dryness of this year
     print('Week', week_pred, 'forecast using model is:', prediction)
     return prediction
+
+# def noIinTEAM(savepath, class_list, obs_week, oneweek_forecasts, twoweek_forecasts, bar_width):
+    # Gettting team names for data collection
+    # team1 = ['Adam', 'Lourdes', 'Patrick', 'Ben']
+    # team2 = ['Alcely', 'Shweta', 'Richard', 'Scott']
+    # team3 = ['Camilo', 'Diana', 'Xenia', 'Danielle']
+    # team4 = ['Alexa', 'Quinn', 'Abigail']
+    # team5 = ['Jill', 'Mekha', 'Jake']
+    # team_names = ['Big_Brain_Squad', 'Team_SARS', 'Aquaholics',
+    #               'Dell_for_the_Win?', 'Team_MJJ']
+    # team_tol = [*team1, *team2, *team3, *team4, *team5]
+
+    # class_pre_dict = pd.DataFrame({'oneweek_forecasts':oneweek_forecasts,
+    #                                'twoweek_forecasts':twoweek_forecasts},
+    #                                index = class_list,
+    #                                columns = ['oneweek_forecasts', 'twoweek_forecasts'])
+
+    # # Organizing by team name
+    # Big_Brain_Squad = class_pre_dict.loc[team1]
+    # Team_SARS = class_pre_dict.loc[team2]
+    # Aquaholics = class_pre_dict.loc[team3]
+    # Dell_for_the_Win = class_pre_dict.loc[team4]
+    # Team_MJJ = class_pre_dict.loc[team5]
+
+    # #Ploting time!
+    # x = np.arange(0, 18, 1)
+    # fig3 = plt.figure()
+    # fig3.set_size_inches(25, 8)
+    # ax = fig3.add_subplot()
+    # w = bar_width
+    # plt.xticks(x + w/2, team_tol, rotation = 60, fontsize=15)
+    # plt.yticks(fontsize=15)
+    # ax.bar(x[0:4], Big_Brain_Squad.oneweek_forecasts, width=w, align='center', label = 'team1')
+    # ax.bar(x[0:4]+w, Big_Brain_Squad.twoweek_forecasts, width=w, align='center', label = 'single1')
+    # ax.bar(x[4:8], Team_SARS.oneweek_forecasts, width=w, align='center', label = 'team2')
+    # ax.bar(x[4:8]+w, Team_SARS.twoweek_forecasts, width=w, align='center', label = 'single2')
+    # ax.bar(x[8:12], Aquaholics.oneweek_forecasts, width=w, align='center', label = 'team3')
+    # ax.bar(x[8:12]+w, Aquaholics.twoweek_forecasts, width=w, align='center', label = 'single3')
+    # ax.bar(x[12:15], Dell_for_the_Win.oneweek_forecasts, width=w, align='center', label = 'team4')
+    # ax.bar(x[12:15]+w, Dell_for_the_Win.twoweek_forecasts, width=w, align='center', label = 'single4')
+    # ax.bar(x[15:18], Team_MJJ.oneweek_forecasts, width=w, align='center', label = 'team5')
+    # ax.bar(x[15:18]+w, Team_MJJ.twoweek_forecasts, width=w, align='center', label = 'single5')
+    # ax.axhline(y=obs_week, linewidth=2, linestyle = '--', color='k')
+    # plt.xlabel('Student', fontsize=15)
+    # plt.ylabel('Average Flow', fontsize=15)
+    # ax.legend( loc='lower center', fontsize=20,
+    #           bbox_to_anchor=(.5, -0.4), ncol=5)
+    # plt.text(0.7, obs_week, 'Observed Flow', fontsize=21)
+    # fig3.patch.set_facecolor('xkcd:white')
+    # plt.tight_layout()
+    # plt.show()
+
+    # fig3.savefig(savepath)
 
 
 # %%
@@ -223,9 +278,9 @@ test = flow_weekly[trainend:][['log_flow',
 
 # %%
 # Step 3a: Fit a linear regression model using sklearn, 1 variable
-b, m, reg_model1, coeff_det1 = mono_reg_mod(train)
+b, m, reg_model1, coeff_det1, q_pred_mono = mono_reg_mod(train)
 # Step 3b: Fit a linear regression model using sklearn, 2 variables
-c, a, reg_model2, coeff_det2 = poly_reg_mod(train)
+c, a, reg_model2, coeff_det2, q_pred_poly = poly_reg_mod(train)
 
 
 # %%
@@ -267,6 +322,8 @@ print(flow_predic_poly(c, a, 16, week_start_flow, forecast_week_1_thru_16))
 
 # Corr_fact1 = week_before_flow / AR1_output
 # Corr_fact2 = week_before_flow / AR2_output
+
+
 # %%
 
 # my_prediction_1 = real_prediction(0, last_week_flow, None)*0.80
@@ -274,4 +331,113 @@ print(flow_predic_poly(c, a, 16, week_start_flow, forecast_week_1_thru_16))
 # print("week 1 prediction outside AR=", my_prediction_1.round(1))
 # print("week 2 prediction outside AR=", my_prediction_2.round(1))
 
+
+# %%
+# 3. Line  plot comparison of predicted and observed flows
+fig1, ax = plt.subplots()
+ax.plot(train['2019-01-01':'2019-12-31'][['log_flow']], color='grey', linewidth=2,
+        label='observed')
+ax.plot(train.index[156:209], q_pred_mono[156:209], color='g', linestyle='--',
+        label='simulated')
+ax.plot(train.index[156:209], q_pred_poly[156:209], color='r', linestyle='--',
+        label='simulated')
+ax.set(title="Observed Log(Flow)", xlabel="Date", ylabel="Weekly Avg Flow [cfs]",
+       yscale='log')
+ax.tick_params(axis='x', labelrotation=70)
+ax.legend()
+# fig1.savefig("graphs/Observed_Log-Flow_Sim.png")
+
+
+# %%
+# Timeseries of the natural log of observed flow values
+# Note that date is the index for the dataframe so it will
+# automatically treat this as our x axis unless we tell it otherwise
+
+fig1, ax = plt.subplots()
+ax.plot(train['2016-01-01':'2016-12-31'][['log_flow']], 'k', label='train_2016')
+ax.plot(train['2017-01-01':'2017-12-31'][['log_flow']], 'b', label='train_2017')
+ax.plot(train['2018-01-01':'2018-12-31'][['log_flow']], 'r', label='train_2018')
+ax.plot(train['2019-01-01':'2019-12-31'][['log_flow']], 'g', label='train_2019')
+ax.set(title="Observed Log(Flow)", xlabel="Date",
+       ylabel="Weekly Avg Log(Flow) [cfs]",
+       yscale='log')
+ax.legend()
+plt.xticks(rotation = 60)
+plt.yticks(np.arange(3, 8, step=0.5))
+fig1.set_size_inches(7, 5)
+# An example of saving your figure to a file
+# fig1.savefig("graphs/Observed_Log-Flow_Train.png")
+
+
+# %%
+# 1. Timeseries of observed flow values
+# Note that date is the index for the dataframe so it will
+# automatically treat this as our x axis unless we tell it otherwise
+
+fig2, ax = plt.subplots()
+ax.plot(flow_weekly['2016-01-01':'2016-12-31'][['flow']], 'k', label='2016')
+ax.plot(flow_weekly['2017-01-01':'2017-12-31'][['flow']], 'b', label='2017')
+ax.plot(flow_weekly['2018-01-01':'2018-12-31'][['flow']], 'r', label='2018')
+ax.plot(flow_weekly['2019-01-01':'2019-12-31'][['flow']], 'g', label='2019')
+ax.set(title="Observed Flow", xlabel="Date",
+       ylabel="Weekly Avg Flow [cfs]")
+ax.legend()
+plt.xticks(rotation = 60)
+fig2.set_size_inches(7, 5)
+# An example of saving your figure to a file
+# fig2.savefig("graphs/Observed_Flow.png")
+
+
+# %%
+# 1. Timeseries of observed flow values
+# Note that date is the index for the dataframe so it will
+# automatically treat this as our x axis unless we tell it otherwise
+# print(len(train['2016-01-01':'2016-12-31'][['log_flow']]))
+# print(len(train['2017-01-01':'2017-12-31'][['log_flow']]))
+# print(len(train['2018-01-01':'2018-12-31'][['log_flow']]))
+# print(len(train['2019-01-01':'2019-12-31'][['log_flow']]))
+x = np.arange(0,52,1)
+x_ly = np.arange(0,53,1)
+
+fig3, ax = plt.subplots()
+ax.plot(x, train['2016-01-01':'2016-12-31'][['log_flow']], 'k', label='train_2016')
+ax.plot(x_ly, train['2017-01-01':'2017-12-31'][['log_flow']], 'b:', label='train_2017')
+ax.plot(x, train['2018-01-01':'2018-12-31'][['log_flow']], 'r', label='train_2018')
+ax.plot(x, train['2019-01-01':'2019-12-31'][['log_flow']], 'g', label='train_2019')
+ax.set(title="Observed Log(Flow)", xlabel="Week Number of the Year",
+       ylabel="Weekly Avg Log(Flow) [cfs]",
+       yscale='log')
+ax.legend()
+plt.xticks(rotation = 60)
+plt.yticks(np.arange(3, 8, step=1))
+fig3.set_size_inches(7, 5)
+# An example of saving your figure to a file
+# fig3.savefig("graphs/Observed_Log-Flow_Train.png")
+
+
+# %%
+# 1. Timeseries of observed flow values
+# Note that date is the index for the dataframe so it will
+# automatically treat this as our x axis unless we tell it otherwise
+# print(len(train['2016-01-01':'2016-12-31'][['log_flow']]))
+# print(len(train['2017-01-01':'2017-12-31'][['log_flow']]))
+# print(len(train['2018-01-01':'2018-12-31'][['log_flow']]))
+# print(len(train['2019-01-01':'2019-12-31'][['log_flow']]))
+x = np.arange(0,52,1)
+x_ly = np.arange(0,53,1)
+
+fig3, ax = plt.subplots()
+ax.plot(x, train['2016-01-01':'2016-12-31'][['log_flow']], 'k', label='train_2016')
+ax.plot(x_ly, train['2017-01-01':'2017-12-31'][['log_flow']], 'b:', label='train_2017')
+ax.plot(x, train['2018-01-01':'2018-12-31'][['log_flow']], 'r', label='train_2018')
+ax.plot(x, train['2019-01-01':'2019-12-31'][['log_flow']], 'g', label='train_2019')
+ax.set(title="Observed Log(Flow)", xlabel="Week Number of the Year",
+       ylabel="Weekly Avg Log(Flow) [cfs]",
+       yscale='log')
+ax.legend()
+plt.xticks(rotation = 60)
+plt.yticks(np.arange(3, 8, step=1))
+fig3.set_size_inches(7, 5)
+# An example of saving your figure to a file
+# fig3.savefig("graphs/Observed_Log-Flow_Train.png")
 # %%
